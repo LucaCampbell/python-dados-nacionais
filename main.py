125dda8b7f460083b09df724d4c1e36c2c5059dc
Lucas Campbell <lcampbell@id.uff.br>
2022-10-04 12:45:06 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from cpf_cnpj import Documento
+from telefone_BR import TelefonesBr
+from datas_br import DatasBr
+from acesso_cep import BuscaEndereco
+
+'''
+cadastro = DatasBr()
+
+print(cadastro)
+
+ex_cnpj = '84725688000167'
+
+documento = Documento.cria_documento(ex_cnpj)
+
+print(documento)
+
+ex_cpf = '01399612670'
+
+cpf_um = Documento.cria_documento(ex_cpf)
+
+print(cpf_um)
+
+telefone = '553399222404'
+
+telefone_objeto = TelefonesBr(telefone)
+
+print(telefone_objeto)
+
+cep = 36902132
+
+objeto_cep = BuscaEndereco(cep)
+
+print(objeto_cep)
+
+a = objeto_cep.acessa_via_cep()
+print(a)
+
+'''
+cadastro = DatasBr()
+
+print(cadastro)
